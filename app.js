27770447a0f2b69b8779d16bb622df75cbd77566
Carlstorm <carlglainnyuysemo@gmail.com>
2025-10-22 04:54:50 +0100

    feat: added the index.js file to create our own endpoint

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
-
+import reconcilationRoute from "./routes/reconcile.routes";
 const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
+//my routes
+
+app.use("/api/v1", reconcilationRoute);
 
 export { app };
